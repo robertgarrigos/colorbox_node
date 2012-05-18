31c7523ed29fd373c5619278ca907743a93b9be0
iLLin <cagedcode@gmail.com>
2012-05-18 11:54:20 -0500

    Issue #[1587652]: Added support for drupal being in a subfolder.

--- colorbox_node.js
+++ colorbox_node.js
@@ -7,9 +7,15 @@
 				var href = $(this).attr('href');
 				// Create an element so we can parse our a URL no matter if its internal or external.
 				var parse = document.createElement('a');
-				parse.href = href;				
-				// Lets prepend our colorbox to the beginning of our link
-				var link = '/colorbox' + parse.pathname;
+				parse.href = href;
+				// Lets add our colorbox link after the base path if necessary.
+				var base_path = Drupal.settings.basePath;
+				if(base_path != '/') {
+					var link = parse.pathname.replace(base_path, base_path + 'colorbox/');
+				} else {
+					var link = base_path + 'colorbox' + parse.pathname;
+				}
+				// Update our href to the link containing colorbox.
 				$(this).attr('href', link);
 			});
 		}
